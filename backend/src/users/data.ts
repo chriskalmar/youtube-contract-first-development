8315b48abf8bdfe6588063b760212d63e07cdb78
Chris Kalmar <christian.kalmar@gmail.com>
2023-10-07 23:15:50 +0200

    provide demo data

---
+++ backend/src/users/data.ts
@@ -0,0 +1,25 @@
+import { User } from './user';
+
+export const userDb: User[] = [
+  {
+    id: 1,
+    email: 'jane@doe.com',
+    name: 'Jane Doe',
+    status: 'Happy',
+    phoneNumbers: ['123-456-7890'],
+  },
+  {
+    id: 2,
+    email: 'brian@doe.com',
+    name: 'Brian Home',
+    status: 'Sad',
+    phoneNumbers: ['234-567-8901'],
+  },
+  {
+    id: 3,
+    email: 'alex@doe.com',
+    name: 'Alex Smith',
+    status: 'Happy',
+    phoneNumbers: ['345-678-9012'],
+  },
+];
