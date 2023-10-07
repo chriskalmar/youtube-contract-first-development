3e5b10163278fdca1a5b85308ca3e6335a4d9e3b
Chris Kalmar <christian.kalmar@gmail.com>
2023-10-07 23:20:06 +0200

    implement user service

---
+++ backend/src/users/usersService.ts
@@ -0,0 +1,28 @@
+import { userDb } from './data';
+import { User } from './user';
+
+export type UserCreationParams = Pick<User, 'email' | 'name' | 'phoneNumbers'>;
+
+export class UsersService {
+  public getAll() {
+    return userDb;
+  }
+
+  public get(id: number, name?: string) {
+    return userDb.find((user) => user.id === id);
+  }
+
+  public create(userCreationParams: UserCreationParams) {
+    const id = userDb.length + 1;
+
+    const newUser: User = {
+      id,
+      status: 'Happy',
+      ...userCreationParams,
+    };
+
+    userDb.push(newUser);
+
+    return newUser;
+  }
+}
