ce029a82dd981ddae0767b85842491b1a883750a
Chris Kalmar <christian.kalmar@gmail.com>
2023-10-08 00:36:12 +0200

    bring list and details together

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,15 @@
+import { UserDetails } from "../components/UserDetails";
 import { UserList } from "../components/UserList";
 
 export default function Home() {
   return (
     <main>
-      <UserList />
+      <section className="bg-purple-100 text-gray-600 h-screen">
+        <div className="flex flex-col h-full">
+          <UserList />
+          <UserDetails />
+        </div>
+      </section>
     </main>
   );
 }
