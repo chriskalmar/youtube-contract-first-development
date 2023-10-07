f4159232dc6fd1d3a3969ab78e6964e4e8dff2d3
Chris Kalmar <christian.kalmar@gmail.com>
2023-10-08 01:17:46 +0200

    convert wrapper into a reusable card component

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 type Props = {
   children: React.ReactNode;
+  title: string;
 };
 
-export const UserDetailsWrapper = ({ children }: Props) => {
+export const Card = ({ children, title }: Props) => {
   return (
     <div className="mt-6 w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
       <div className="px-5 py-4 border-b border-gray-100">
-        <h2 className="font-semibold text-gray-800">User Details</h2>
+        <h2 className="font-semibold text-gray-800">{title}</h2>
       </div>
       <div className="p-3">{children}</div>
     </div>
